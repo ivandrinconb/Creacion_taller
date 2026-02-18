647ffb5799456b747d13d0179aa6ca12a4feb818
ivandrinconb <ivand.rinconb@uqvirtual.edu.co>
2026-02-17 23:37:31 -0500

    adelanto de codigo

--- src/main/java/co/edu/uniquindio/taller/tallerapp/controller/ClienteController.java
+++ src/main/java/co/edu/uniquindio/taller/tallerapp/controller/ClienteController.java
@@ -1,4 +1,49 @@
 package co.edu.uniquindio.taller.tallerapp.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class ClienteController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button btnGuardar;
+
+    @FXML
+    private TextField txtDireccion;
+
+    @FXML
+    private TextField txtIdentificacion;
+
+    @FXML
+    private TextField txtNombre;
+
+    @FXML
+    private TextField txtTelefono;
+
+    @FXML
+    void onGuardarCliente(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert btnGuardar != null : "fx:id=\"btnGuardar\" was not injected: check your FXML file 'ClienteView.fxml'.";
+        assert txtDireccion != null : "fx:id=\"txtDireccion\" was not injected: check your FXML file 'ClienteView.fxml'.";
+        assert txtIdentificacion != null : "fx:id=\"txtIdentificacion\" was not injected: check your FXML file 'ClienteView.fxml'.";
+        assert txtNombre != null : "fx:id=\"txtNombre\" was not injected: check your FXML file 'ClienteView.fxml'.";
+        assert txtTelefono != null : "fx:id=\"txtTelefono\" was not injected: check your FXML file 'ClienteView.fxml'.";
+
+    }
+
 }
+
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,10 @@ module co.edu.uniquindio.taller.tallerapp {
 
     opens co.edu.uniquindio.taller.tallerapp to javafx.fxml;
     exports co.edu.uniquindio.taller.tallerapp;
+
+    opens  co.edu.uniquindio.taller.tallerapp.controller;
+    exports co.edu.uniquindio.taller.tallerapp.controller;
+
+    opens  co.edu.uniquindio.taller.tallerapp.model;
+    exports co.edu.uniquindio.taller.tallerapp.model;
 }
